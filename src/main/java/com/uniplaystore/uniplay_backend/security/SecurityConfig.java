b04c3abdd9729719368d832e66fcb9d9ed156173
matheus <matheus.holanda101@gmail.com>
2025-06-08 21:02:46 -0300

    Correção do CORS Cross origin

--- src/main/java/com/uniplaystore/uniplay_backend/security/SecurityConfig.java
+++ src/main/java/com/uniplaystore/uniplay_backend/security/SecurityConfig.java
@@ -72,7 +72,8 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
 
-        config.addAllowedOrigin("uniplay-store-2cf7ku3mj-matheus-projects-5617f3c4.vercel.app");
+        config.addAllowedHeader("Access-Control-Allow-Origin:https://uniplay-store.vercel.app");
+        config.addAllowedOrigin("https://uniplay-store.vercel.app");
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
