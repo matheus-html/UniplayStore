9f9a424bcbb04ac096172694999ad86b1e855fdd
matheus <matheus.holanda101@gmail.com>
2025-06-09 00:30:24 -0300

    Liga Railway

--- src/main/java/com/uniplaystore/uniplay_backend/UniplayBackendApplication.java
+++ src/main/java/com/uniplaystore/uniplay_backend/UniplayBackendApplication.java
@@ -3,9 +3,6 @@ package com.uniplaystore.uniplay_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.modulith.ApplicationModule;
-import org.springframework.context.annotation.Bean;
-//import org.springframework.modulith.ApplicationModules;
-import org.springframework.modulith.docs.Documenter;
 
 @SpringBootApplication
 @ApplicationModule(type = ApplicationModule.Type.OPEN)
@@ -15,12 +12,5 @@ public class UniplayBackendApplication {
 		SpringApplication.run(UniplayBackendApplication.class, args);
 	}
 
-//	@Bean
-//	ApplicationModules applicationModules() {
-//		ApplicationModules modules = ApplicationModules.of(GameStoreApplication.class);
-//		new Documenter(modules)
-//				.writeDocumentation(); // Gera diagramas C4 e UML
-//		return modules;
-//	}
 
 }
